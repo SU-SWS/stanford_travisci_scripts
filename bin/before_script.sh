7d14bd1046f7ab3b0f6780195dd8b0552f693a9b
Kellie Brownell <kbrownel@stanford.edu>
2017-01-06 11:36:42 -0800

    Adding SWS Contexts to behat features
    fixup! Run behat with vvv

--- bin/before_script.sh
+++ bin/before_script.sh
@@ -6,6 +6,6 @@ TEST_FEATURE=$(ls *.info | cut -f1 -d".")
 git clone https://github.com/SU-SWS/linky_clicky.git $BASEDIR/linky_clicky
 cd $BASEDIR/linky_clicky
 mkdir sites/$TEST_FEATURE
-mv $BASEDIR/features sites/$TEST_FEATURE
-mv $BASEDIR/behat.yml sites/$TEST_FEATURE
+mv $BASEDIR/features sites/$TEST_FEATURE/.
+mv $BASEDIR/behat.yml sites/$TEST_FEATURE/.
 cp sites/uat/features/$TEST_FEATURE*.feature $BASEDIR/linky_clicky/sites/$TEST_FEATURE/features/.
--- bin/script.sh
+++ bin/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd $BASEDIR/linky_clicky/sites/$TEST_FEATURE
-$BASEDIR/vendor/bin/behat -p default -s all features
+behat -vvv -p default -s all features
