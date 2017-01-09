3d76892008a17babc81b4e3cd3563646c9b890eb
Kellie Brownell <kbrownel@stanford.edu>
2017-01-09 15:24:50 -0800

    Remove tests from previous builds

--- bin/before_script.sh
+++ bin/before_script.sh
@@ -6,6 +6,8 @@ cd ../
 TEST_FEATURE=$(ls *.info | cut -f1 -d".")
 # download linky_clicky and copy over related tests
 git clone https://github.com/SU-SWS/linky_clicky.git $BASEDIR/linky_clicky
+# remove previously tested features
+rm $BASEDIR/features/*.feature
 cp $BASEDIR/linky_clicky/includes/features/SU-SWS/$TEST_FEATURE/$TEST_FEATURE.feature $BASEDIR/features/.
 # start xvfb virtual display
 export DISPLAY=:99.0
