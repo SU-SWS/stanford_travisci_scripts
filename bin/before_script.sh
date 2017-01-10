00e1899e8b30dacb93b3f06dfc26559944443948
Kellie Brownell <kbrownel@stanford.edu>
2017-01-10 10:32:57 -0800

    Output paths and ls to check work

--- bin/before_script.sh
+++ bin/before_script.sh
@@ -4,8 +4,11 @@
 # download linky_clicky and copy over related tests
 git clone https://github.com/SU-SWS/linky_clicky.git
 cp linky_clicky/includes/features/SU-SWS/$TEST_FEATURE/$TEST_FEATURE.feature stanford_travisci_scripts/features/.
+ls stanford_travisci_scripts/features
+
 # start xvfb virtual display
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
+
 # give xvfb time to launch before running tests
 sleep 3
