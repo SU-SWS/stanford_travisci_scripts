1b1421ed87b49d307696888f3c0afc060521d2f0
Kellie Brownell <kbrownel@stanford.edu>
2017-01-18 20:33:30 -0800

    Copy over linky_clicky's context files.
    Instead of providing your own and keeping them in sync copy over linky_cliky's context files. The one missing php file (FeatureContext.php) is usually an empty class and can be kept in this repo.
    
    Removing unnecessary files
    
    Updating paths for linky_clicky files
    
    Updating .yml file reflect build directory paths
    
    Add composer path to bashrc
    
    Troubleshoot remote issue with bashrc
    
    Export composer path to .profile
    
    Sourcing .profile
    
    Creating drush alias
    
    Source bashrc in install
    
    Trying alias to local drush
    
    export path in each script
    
    Export path in each script
    
    Start webserver in install.sh
    
    fixup! cd .. after netstat
    
    Change runserver to use builtin
    
    Change port to 8085
    
    Kill runserver after tests
    
    Updating behat includes path
    
    Run behat from stanford_travisci_scripts
    
    Trying selenium with chrome
    
    Moving chromedriver to user/bin
    
    Updating chromedriver
    
    Don't wait for netstat
    
    Updates after successful local run
    
    Start server with drush alias
    
    Removing netstat listen
    
    Changing port back to 8080
    
    Trying netstat on same line
    
    Run with sudo
    
    Runserver does not work with aliases
    
    Updating selenium version
    
    Starting server after install
    
    Selenium runs behat tests
    
    Check port 8080 and 4444
    
    Start php runserver in html

--- bin/before_script.sh
+++ bin/before_script.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 
 # before_script
-# download linky_clicky and copy over related tests
-git clone https://github.com/SU-SWS/linky_clicky.git
-cp linky_clicky/includes/features/SU-SWS/$TEST_FEATURE/$TEST_FEATURE.feature stanford_travisci_scripts/features/.
-cp linky_clicky/includes/bootstrap/* stanford_travisci_scripts/features/bootstrap/
-ls stanford_travisci_scripts/features
+export PATH="$HOME/.composer/vendor/bin:$PATH"
+# download linky_clicky and copy over related tests and required files
+git clone --depth 1 https://github.com/SU-SWS/linky_clicky.git
+mkdir -p stanford_travisci_scripts/includes/config
+mkdir stanford_travisci_scripts/includes/extensions
+cp -r linky_clicky/includes/features/SU-SWS/$TEST_FEATURE stanford_travisci_scripts/features/.
+cp linky_clicky/includes/bootstrap/* stanford_travisci_scripts/features/bootstrap/.
+cp linky_clicky/includes/config/default.yml stanford_travisci_scripts/includes/config/.
+cp linky_clicky/includes/extensions/drupal.extension.yml stanford_travisci_scripts/includes/extensions/.
+cp linky_clicky/includes/extensions/mink.extension.yml stanford_travisci_scripts/includes/extensions/.
 
 # start xvfb virtual display
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 
-# give xvfb time to launch before running tests
+# give xvfb time to launch before proceeding
 sleep 3
+
+# install net-tools
+sudo apt-get install -y net-tools
+
+# kill persistent proccesses from previous builds
+# kill processes on port used by selenium-server
+kill $(lsof -ti tcp:4444)
+# kill processes on port used by php runserver
+kill $(lsof -t -i:8080)
+
+# start php runserver from within site directory
+# alias failed to find webroot
+cd html
+drush runserver 127.0.0.1:8080 &
+# wait until server is up and running before proceeding
+until netstat -an 2>/dev/null | grep '8080.*LISTEN'; do true; done
+cd ..
+
+# download recommended version of selenium-server, start and background process
+wget http://selenium-release.storage.googleapis.com/2.47/selenium-server-standalone-2.47.1.jar
+java -jar selenium-server-standalone-2.47.1.jar -p 4444 &>/dev/null &
+
+# wait until selenium-server is up and running before proceeding
+until netstat -an 2>/dev/null | grep '4444.*LISTEN'; do true; done
--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # install
-git clone --depth 1 -b travis https://github.com/SU-SWS/Stanford-Drupal-Profile.git Stanford-Drupal-Profile
+export PATH="$HOME/.composer/vendor/bin:$PATH"
+git clone --depth 1 -b travis https://github.com/SU-SWS/Stanford-Drupal-Profile.git
 
 # download site files in travis build directory
 drush make -y --force-complete Stanford-Drupal-Profile/make/dept.make html
@@ -10,6 +11,8 @@ drush make -y --force-complete Stanford-Drupal-Profile/make/dept.make html
 sed -e "s|BUILD_DIR|${TRAVIS_BUILD_DIR}|" stanford_travisci_scripts/aliases.drushrc.php > ~/.drush/aliases.drushrc.php
 # install site with stanford self-service profile
 drush @local si -y stanford --db-url=mysql://root@localhost/drupal --account-name=admin --account-pass=admin
+sed -ie "s|# RewriteBase /|RewriteBase /|" html/.htaccess
 
 # disable webauth module and uncomment RewriteBase
 drush @local dis -y webauth
+
