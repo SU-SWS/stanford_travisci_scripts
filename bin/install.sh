150e78834f8538d782c01ecd85a37f1b72b1c690
Kellie Brownell <kbrownel@stanford.edu>
2017-02-01 11:17:15 -0800

    Clone deployer

--- bin/install.sh
+++ bin/install.sh
@@ -11,11 +11,12 @@ sed -ie "s|TRAVIS_BUILD_DIR|$TRAVIS_BUILD_DIR|" $HOME/.drush/aliases.drushrc.php
 cat $HOME/.drush/aliases.drushrc.php
 
 if [ -z "$PRODUCT_NAME" ]; then
-  git clone --depth 1 -b travis https://github.com/SU-SWS/Stanford-Drupal-Profile.git
-  drush make -y --force-complete Stanford-Drupal-Profile/make/dept.make html
+  git clone --depth 1 -b travis https://github.com/SU-SWS/Stanford-Drupal-Profile.git $HOME/Stanford-Drupal-Profile
+  drush make -y --force-complete $HOME/Stanford-Drupal-Profile/make/dept.make html
   drush @local si -y stanford --db-url=mysql://root@localhost/drupal --account-name=admin --account-pass=admin
 else
-  drush make -y --force-complete production/product/$PRODUCT_NAME/$PRODUCT_NAME.make html
+  git clone --depth 1 -b proboci https://github.com/SU-SWS/stanford-jumpstart-deployer.git $HOME/stanford-jumpstart-deployer
+  drush make -y --force-complete $HOME/stanford-jumpstart-deployer/production/product/$PRODUCT_NAME/$PRODUCT_NAME.make html
   PROFILE_NAME=$(find $TRAVIS_BUILD_DIR/html/profiles -name "*jumpstart*" -type d -printf '%f\n')
   drush @local si -y $PROFILE_NAME --db-url=mysql://root@localhost/drupal --account-name=admin --account-pass=admin
 fi
