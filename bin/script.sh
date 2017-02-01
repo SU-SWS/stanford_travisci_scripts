386a1636b551a8a530bd81801eb50ecd753f26cd
Kellie Brownell <kbrownel@stanford.edu>
2017-02-01 11:44:27 -0800

    Updates to tracking and printing of failed tests

--- bin/script.sh
+++ bin/script.sh
@@ -8,15 +8,13 @@ cd $HOME/stanford_travisci_scripts
 BEHAT_TESTS=$(find features -name "*.feature" -type f -printf '%f\n')
 echo "$BEHAT_TESTS"
 FAILURES_COUNT=0
-FAILURES_DETAIL=()
 
 # reinstall site once after first behat test failure
 function reinstall_rerun_on_first_failure {
-  drush @local si stanford -y
+  drush @local si $PROFILE_NAME -y
   TEST_RESULT=$(timeout 3m bin/behat -p default -s all -f pretty features/$REPOSITORY_NAME/$TEST 1>&2)
   if [[ $TEST_RESULT == *"Failed"* ]] || [[ $TEST_RESULT == *"Terminated"* ]]; then
     ((FAILURES_COUNT++))
-    FAILURES_DETAIL+=$TEST_RESULT
   fi
 }
 
@@ -24,7 +22,7 @@ function reinstall_rerun_on_first_failure {
 # increment failure count if test failed
 function evaluate_first_test_result {
   if [[ $TEST_RESULT == *"Failed"* ]] || [[ $TEST_RESULT == *"Terminated"* ]]; then
-    reinstall-rerun_on_first_failure
+    reinstall_rerun_on_first_failure
   fi
 }
 
@@ -35,10 +33,10 @@ for TEST in ${BEHAT_TESTS[@]}; do
   evaluate_first_test_result
 done
 
-echo "${FAILURES_DETAIL[*]}"
+echo $FAILURES_COUNT
 
 # fail script.sh if behat returned at least one failure
-if [[ FAILURES_COUNT > 0 ]]; then
+if [ FAILURES_COUNT > 0 ]; then
   exit 1
 else
   exit 0
