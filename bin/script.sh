c65f8a4f64693db4660fc1526c09ff85ee37734e
kbrownell <kbrownell@users.noreply.github.com>
2017-02-10 16:09:11 -0800

    Removing timeout

--- bin/script.sh
+++ bin/script.sh
@@ -4,7 +4,7 @@
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 
 # run through all tests in features directory
-timeout 3m bin/behat -p default -s dev features
+bin/behat -p default -s dev features
 
 # grap the number of failures from behat's html output summary report
 FAILURES_COUNT=$(cat behat_results/index.html | grep 'scenarios failed of' | sed -r 's/^([^.]+).*$/\1/; s/^[^0-9]*([0-9]+).*$/\1/')
