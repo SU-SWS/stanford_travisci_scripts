12ed0e5078cda19d7f464f6bb0b0b408925f9d8f
Kellie Brownell <kbrownel@stanford.edu>
2017-02-24 14:55:21 -0800

    fixup! Remove basename from test count

--- bin/script.sh
+++ bin/script.sh
@@ -10,7 +10,7 @@ bin/behat -p default -s dev features
 
 # grap the number of failures from behat's html output summary report
 FAILURES_COUNT=$(cat behat_results/index.html | grep 'scenarios failed of' | sed -r 's/^([^.]+).*$/\1/; s/^[^0-9]*([0-9]+).*$/\1/')
-TESTS_COUNT=$(basename `find $HOME/stanford_travisci_scripts/features/$REPOSITORY_NAME -type f -name "*.feature"` | grep -c ".feature")
+TESTS_COUNT=$(find $HOME/stanford_travisci_scripts/features/$REPOSITORY_NAME -type f -name "*.feature" | grep -c ".feature")
 
 echo "Number of failed tests: $FAILURES_COUNT"
 echo "Number of tests counted: $TESTS_COUNT"
