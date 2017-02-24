3a70fc7e5871898cafee0c8ed3c1b05a1cdc345c
Kellie Brownell <kbrownel@stanford.edu>
2017-02-24 13:57:37 -0800

    fixup! Count features with grep

--- bin/script.sh
+++ bin/script.sh
@@ -2,6 +2,7 @@
 
 # script
 export PATH="$HOME/.composer/vendor/bin:$PATH"
+export REPOSITORY_NAME=$(basename $TRAVIS_BUILD_DIR)
 
 # run through all tests in features directory
 cd $HOME/stanford_travisci_scripts
@@ -9,13 +10,13 @@ bin/behat -p default -s dev features
 
 # grap the number of failures from behat's html output summary report
 FAILURES_COUNT=$(cat behat_results/index.html | grep 'scenarios failed of' | sed -r 's/^([^.]+).*$/\1/; s/^[^0-9]*([0-9]+).*$/\1/')
-TESTS_COUNT=$(cat features/*/*.feature | grep -c "Scenario")
+TESTS_COUNT=$(basename `find $HOME/stanford_travisci_scripts/features/$REPOSITORY_NAME -type f -name "*.feature"` | grep -c ".feature")
 
 echo "Number of failed tests: $FAILURES_COUNT"
 echo "Number of tests counted: $TESTS_COUNT"
 
 # fail script.sh if behat returned at least one failure
-if (( $FAILURES_COUNT > 0 )) || (( $TESTS_COUNT == 0 )); then
+if (( $FAILURES_COUNT > 0 )) || (( $TESTS_COUNT > 0 )); then
   exit 1
 else
   exit 0
