769657461bddd643a71b8ac453508ab21d518e29
Kellie Brownell <kbrownel@stanford.edu>
2017-03-02 09:49:41 -0800

    Fail if FAILURES_COUNT is empty

--- bin/script.sh
+++ bin/script.sh
@@ -15,7 +15,7 @@ FEATURE_FILES=$(find $HOME/stanford_travisci_scripts/features/$REPOSITORY_NAME -
 echo "Number of failed tests: $FAILURES_COUNT"
 
 # fail script.sh if behat returned at least one failure
-if (( $FAILURES_COUNT > 0 )) || [ -z "$FEATURE_FILES" ]; then
+if (( $FAILURES_COUNT > 0 )) || [ -z "$FAILURES_COUNT" ] || [ -z "$FEATURE_FILES" ]; then
   exit 1
 else
   exit 0
