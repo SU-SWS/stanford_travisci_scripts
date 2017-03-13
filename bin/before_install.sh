fedb7336c16a075a447a6e0638ce66c72a18addd
Kellie Brownell <kbrownel@stanford.edu>
2017-03-13 13:44:38 -0700

    Upgrade to drush 8

--- bin/before_install.sh
+++ bin/before_install.sh
@@ -3,7 +3,7 @@
 # before_install
 cd $HOME/stanford_travisci_scripts
 composer install
-composer global require drush/drush:7.1.0
+composer global require drush/drush:8.1.10
 phpenv config-rm xdebug.ini
 phpenv rehash
 cd $HOME
