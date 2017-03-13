fde70b32dd9ec51a4dfa58e0fd54a539734a7475
Kellie Brownell <kbrownel@stanford.edu>
2017-03-13 15:09:46 -0700

    Exclude tests requiring email

--- bin/script.sh
+++ bin/script.sh
@@ -6,7 +6,7 @@ export REPOSITORY_NAME=$(basename $TRAVIS_BUILD_DIR)
 
 # run through all tests in features directory
 cd $HOME/stanford_travisci_scripts
-bin/behat -p default -s dev --tags '~@sites&&~@webauth' features
+bin/behat -p default -s dev --tags '~@sites&&~@webauth&&~@email' features
 
 # grap the number of failures from behat's html output summary report
 FAILURES_COUNT=$(cat behat_results/index.html | grep 'scenarios failed of' | sed -r 's/^([^.]+).*$/\1/; s/^[^0-9]*([0-9]+).*$/\1/')
