6a47697c2d0f923d02327e0a7bc48d6e3942dee0
Kellie Brownell <kbrownel@stanford.edu>
2017-03-31 13:26:49 -0700

    Changes after testing locally

--- bin/install.sh
+++ bin/install.sh
@@ -14,19 +14,18 @@ sed -ie "s|HOME|$HOME|" $HOME/.drush/aliases.drushrc.php
 cat $HOME/.drush/aliases.drushrc.php
 
 # install d8 site
-git clone --depth 1 https://github.com/SU-SWS/j8 $HOME/html
-cd j8/
+git clone --depth 1 https://github.com/SU-SWS/j8 $HOME/j8
+cd $HOME/j8
 composer install --prefer-source
 ln -s $HOME/j8/web $HOME/html
 
+# install site
+bin/drupal site:install stanford_sites_jumpstart --force --no-interaction --langcode="en" --db-type="mysql" --db-host="127.0.0.1" --db-name="drupal8" --db-user="root" --db-pass="" --db-port="3306" --site-name="J8" --site-mail="admin@example.com" --account-name="admin" --account-mail="admin@example.com" --account-pass="admin"
+
 # Place the base path in the settings.php file because it has a non default port.
 chmod 0777 $HOME/html/sites/default/settings.php
-sudo echo "\$base_url = \"http://127.0.0.1:8080\";" >> $HOME/html/sites/default/settings.php
-sudo echo d8_settings.php >> $HOME/html/web/sites/default/settings.php
+sudo echo "\$base_url = \"http://127.0.0.1:8888\";" >> $HOME/html/sites/default/settings.php
 chmod 0644 $HOME/html/sites/default/settings.php
-
-# install site
-bin/drupal site:install stanford_sites_jumpstart --force
 
 # Adjust the rewrite base for the local host and enable clean url's.
 sed -ie "s|# RewriteBase /|RewriteBase /|" $HOME/html/.htaccess
@@ -36,9 +35,17 @@ drush @local vset clean_url 1
 # Ensure that files is writable.
 chmod 0777 $HOME/html/sites/default/files
 
-# Disable modules based on testing requirements.
-if [ ! -z "$DISABLE_MODULES" ]; then
-  drush @local dis -y "$DISABLE_MODULES"
+# start php runserver silently and with custom router
+# must be started in the site's root directory
+cp $HOME/stanford_travisci_scripts/routing.php $HOME/html/routing.php
+cd $HOME/html
+php -S 127.0.0.1:8888 $HOME/html/routing.php &>/dev/null &
+# wait until server is up and running before proceeding
+until netstat -an 2>/dev/null | grep '8080.*LISTEN'; do true; done
+cd ..
+
+if (`curl --silent http://127.0.0.1:8888 | grep "block-stanford-basic-branding"`); then
+  exit 0
+else
+  exit 1
 fi
-
-
