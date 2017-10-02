dd83588fa589fad323a53069e604b649544ad40b
Kellie Brownell <kbrownel@stanford.edu>
2017-10-02 10:56:23 -0700

    Run behat tests with headless chrome

--- bin/before_script.sh
+++ bin/before_script.sh
@@ -65,6 +65,23 @@ php -S 127.0.0.1:8080 $HOME/html/routing.php &>/dev/null &
 until netstat -an 2>/dev/null | grep '8080.*LISTEN'; do true; done
 cd ..
 
+# download and install Chrome
+sudo apt-get install libxss1 libappindicator1 libindicator7 vim wget -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# Unpackaging chrome returns an error because it is missing dependencies, which is not a problem,
+# We install them a moment later.  So adding || true to be sure this behavior does not quite the script.
+sudo dpkg -i google-chrome-stable_current_amd64.deb || true
+sudo apt-get -f install -y
+google-chrome --version
+
+status "Installing Chromedriver"
+wget https://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv -f chromedriver /usr/local/share/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
+
 # download recommended version of selenium-server, start, silence, and background process
 wget http://selenium-release.storage.googleapis.com/2.47/selenium-server-standalone-2.47.1.jar
 java -jar selenium-server-standalone-2.47.1.jar -p 4444 &>/dev/null &
